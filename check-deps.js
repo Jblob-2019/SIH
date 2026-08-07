3260f8942f4ed453b15fbbc41ed5e85f04d5e32d
8319janarthanans-code <8319.janarthanan.s@gmail.com>
2026-08-08 03:01:40 +0530

    fs

--- check-deps.js
+++ check-deps.js
@@ -2,12 +2,32 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 
 function checkAndInstall(dir) {
-  if (!fs.existsSync(`${dir}/node_modules`)) {
-    console.log(`[Setup] Missing dependencies in '${dir}'. Installing now...`);
-    // Run npm install for the specific directory
+  let missing = false;
+  
+  if (fs.existsSync(`${dir}/package.json`)) {
+    try {
+      const pkg = JSON.parse(fs.readFileSync(`${dir}/package.json`, 'utf8'));
+      const deps = { ...(pkg.dependencies || {}), ...(pkg.devDependencies || {}) };
+      
+      for (const dep of Object.keys(deps)) {
+        if (!fs.existsSync(`${dir}/node_modules/${dep}`)) {
+          missing = true;
+          break;
+        }
+      }
+    } catch (e) {
+      missing = true; // Fallback to install on read error
+    }
+  } else {
+    // If no package.json, we don't need to install
+    return;
+  }
+
+  if (missing || !fs.existsSync(`${dir}/node_modules`)) {
+    console.log(`[Setup] Missing or incomplete dependencies in '${dir}'. Installing now...`);
     execSync(dir === '.' ? 'npm install' : `npm --prefix ${dir} install`, { stdio: 'inherit' });
   } else {
-    console.log(`[Setup] Dependencies already exist in '${dir}'. Skipping download.`);
+    console.log(`[Setup] Dependencies verified successfully in '${dir}'.`);
   }
 }
 
